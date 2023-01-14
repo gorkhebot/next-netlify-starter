d44c6994ab3cc0c7a9ffb75b6de038569e19e1c5
Hari Acharya <hyaree@Haris-MacBook-Pro.local>
2023-01-14 13:19:00 -0600

    initial

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome to the world of only kk fans!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by uploading... <code>pages/index.js</code>
         </p>
       </main>
 
